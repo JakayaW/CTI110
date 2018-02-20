745b8971904dbb8f0665d58dc7c3c1c247eae70c
JakayaW <36679437+JakayaW@users.noreply.github.com>
2018-02-20 16:22:25 -0500

    Add files via upload
    First Submission

---
+++ P2HW1_DistanceTraveled_WigginsJakaya.py
@@ -0,0 +1,13 @@
+#CTI-110
+#P2HW1 - Distance Traveled
+#Jakaya Wiggins
+#2/20/2018
+speed = 70
+print( "In 6 hours, the car will travel " + str( (speed * 6)) + "miles")
+print( "In 6 hours, the car will travel " + str( (speed * 10)) + "miles")
+print( "In 6 hours, the car will travel " + str( (speed * 15)) + "miles")
+
+In 6 hours, the car will travel 420miles
+In 6 hours, the car will travel 700miles
+In 6 hours, the car will travel 1050miles
+>>> 
